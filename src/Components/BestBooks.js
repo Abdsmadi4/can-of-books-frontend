1c45a8408558c8f8abe591f3230606a60b525794
Abdallah Smadi <abd.smadi41@gmail.com>
2022-08-09 22:21:40 +0300

    lab 11

--- src/Components/BestBooks.js
+++ src/Components/BestBooks.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import Card from 'react-bootstrap/Card';
+import Carousel from 'react-bootstrap/Carousel';
+import axios from 'axios';
+
 
 class BestBooks extends React.Component {
   constructor(props) {
@@ -9,31 +11,46 @@ class BestBooks extends React.Component {
     }
   }
 
-  /* TODO: Make a GET request to your API to fetch all the books from the database  */
+  getBooks = async () => {
+    const res = await axios.get('http://localhost:8080/books');
+    this.setState({
+      books: res.data
+    });
+  };
+
+  componentDidMount() {
+    console.log('BestBooks componentDidMount');
+    this.getBooks();
+  }
+
 
   render() {
 
-    /* TODO: render all the books in a Carousel */
 
     return (
       <>
-        <Card style={{ width: '18rem' }}>
-          <Card.Body>
-            <Card.Title>My Favorite Books</Card.Title>
-            <Card.Subtitle className="mb-2 text-muted">This is a collection of my favorite books</Card.Subtitle>
-            {this.state.books.length ? (
-              <Card.Text>Book Carousel coming soon</Card.Text>
-            ) : (
-              <Card.Text>No Books Found :(</Card.Text>
-            )}
-          </Card.Body>
-        </Card>
+
+        {this.state.books.length ?
+          <Carousel fade variant="dark" style={{ width: '40%', height: '40%' }}>
+            {this.state.books.map((book, index) => (
+              <Carousel.Item key={index}>
+                <img
+                  className="d-block w-100"
+                  src={book.image}
+                  alt={book.title}
+                />
+                <Carousel.Caption>
+                  <h3>{book.title}</h3>
+                  <p>{book.description}</p>
+                </Carousel.Caption>
+              </Carousel.Item>
+            ))}
+          </Carousel> : <h3>Book Collection is Empty</h3>}
       </>
     )
   }
 }
 
 export default BestBooks;
-
 
 
